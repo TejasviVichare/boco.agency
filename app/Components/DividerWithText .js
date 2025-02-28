4db4ef65695f9f9eb19523e80f0a3a1c4aa4e845
TejasviVichare <tejasvivichare2121@gmail.com>
2025-02-28 12:45:28 +0530

    Updated

--- app/Components/DividerWithText .js
+++ app/Components/DividerWithText .js
@@ -1,16 +1,15 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons'; // ✅ Correct import
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const DividerWithText = () => {
   return (
     <div className="relative flex items-center my-4">
       {/* Smaller HR */}
-      <hr className="w-28 border-gray-300" />
+      <hr className="w-8 border-gray-300" />
 
-
-      <div className="flex items-center bg-white px-3 whitespace-nowrap flex-shrink-0 gap-3">
+      <div className="flex flex-row md:flex-row items-center text-center bg-white px-3 whitespace-nowrap flex-shrink-0 gap-2 md:gap-3">
         {/* Stars */}
-        <div className="flex ">
+        <div className="flex">
           <FontAwesomeIcon icon={faStar} className="text-yellow-500 text-xl flex-shrink-0 w-6" />
           <FontAwesomeIcon icon={faStar} className="text-yellow-500 text-xl flex-shrink-0 w-6" />
           <FontAwesomeIcon icon={faStar} className="text-yellow-500 text-xl flex-shrink-0 w-6" />
@@ -18,8 +17,7 @@ const DividerWithText = () => {
           <FontAwesomeIcon icon={faStar} className="text-yellow-500 text-xl flex-shrink-0 w-6" />
         </div>
 
-    
-        <span className="md:text-xl text-sm font-medium" style={{ color: '#140152' }}>
+        <span className="md:text-xl text-sm font-medium" style={{ color: "#140152" }}>
           90+ Brands & Counting
         </span>
       </div>
--- app/Components/Talktous.js
+++ app/Components/Talktous.js
@@ -5,7 +5,7 @@ const Talktous = ({text}) => {
     <>
          <div className=' md:flex flex-row border rounded-full  text-white p-2 md:pl-6 pl-3' style={{backgroundColor:'#140152'}}>            
          <Link href="/" className='text-lg font-semibold pr-3 align-middle mt-2'>{text}</Link>
-         <span className='border rounded-full bg-white text-indigo-950 md:py-1 py-1 md:px-3 px-1 md:text-3xl text-lg '> &#8599;</span>
+         <span className='border rounded-full bg-white text-indigo-950 md:py-1 py-1 md:px-3 px-1 md:text-3xl text-lg text-right'> &#8599;</span>
          </div>      
     </>
   )
